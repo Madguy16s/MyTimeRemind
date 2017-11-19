e1449adaf49a788535297e8354656e0517f32ec1
jason <jason.gumarjadi@gmail.com>
2017-11-18 16:44:00 -0800

    Initial commit

---
+++ app/src/main/java/com/example/jason/mytimeremind/AddContact.java
@@ -0,0 +1,17 @@
+package com.example.jason.mytimeremind;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jason on 15-Nov-17.
+ */
+
+public class AddContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_contact);
+    }
+}
---
+++ app/src/main/java/com/example/jason/mytimeremind/SettingActivity.java
@@ -0,0 +1,16 @@
+package com.example.jason.mytimeremind;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jason on 15-Nov-17.
+ */
+
+public class SettingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_activity);
+    }
+}
